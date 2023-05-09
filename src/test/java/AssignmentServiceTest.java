bc1c7b12f965992188475b967b260d89371bfc1c
teodor-front <teodor@getfront.com>
2023-05-09 20:12:56 +0300

    L4_TH add mockito tests

--- src/test/java/AssignmentServiceTest.java
+++ src/test/java/AssignmentServiceTest.java
@@ -3,6 +3,11 @@ import domain.Student;
 import domain.Tema;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import repository.*;
 import service.Service;
 import validation.NotaValidator;
@@ -11,15 +16,36 @@ import validation.TemaValidator;
 import validation.Validator;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
 
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class AssignmentServiceTest {
-    private Service service;
+    Service service;
+
+    @Mock
+    Validator<Student> studentValidatorMock;
+    @Mock
+    Validator<Tema> temaValidatorMock;
+    @Mock
+    Validator<Nota> notaValidatorMock;
+    @Mock
+    StudentRepository mockedStudentRepo;
+
+    @Mock
+    NotaRepository mockedNotaRepo;
+
+    @Mock
+    TemaRepository mockedTemaRepo;
+
+    @InjectMocks
+    Service serviceWithMocks;
+
 
     private String assignmentId = "123";
     private String assignmentDescription = "1233";
     private int assignmentDeadline = 3;
     private int assignmentStartLin = 2;
-
 
     @Before
     public void setup(){
@@ -27,14 +53,14 @@ public class AssignmentServiceTest {
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
 
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
         StudentRepository studentRepository = new StudentRepository(studentValidator);
         NotaRepository notaRepository = new NotaRepository(notaValidator);
         TemaRepository temaRepository = new TemaRepository(temaValidator);
 
         this.service = new Service(studentRepository, temaRepository, notaRepository);
+
+        serviceWithMocks = new Service(mockedStudentRepo, mockedTemaRepo, mockedNotaRepo);
     }
 
     @Test
@@ -77,5 +103,19 @@ public class AssignmentServiceTest {
         service.saveTema(assignmentId, assignmentDescription, assignmentDeadline, assignmentStartLin);
         int result = service.saveTema(assignmentId, assignmentDescription, assignmentDeadline, assignmentStartLin);
         assertEquals(1, result);
+    }
+
+    @Test
+    public void addAssignmentIntegrationTest() {
+        Student student = new Student("1", "1", 912);
+        Tema tema = new Tema("1123230", "tema 1", 3, 1);
+        doNothing().when(studentValidatorMock).validate(any());
+        doNothing().when(temaValidatorMock).validate(any());
+        Mockito.when(mockedStudentRepo.exists("1")).thenReturn(false);
+        Mockito.when(mockedTemaRepo.exists(tema.getID())).thenReturn(false);
+        Mockito.when(mockedStudentRepo.save(student)).thenReturn(student);
+        Mockito.when(mockedTemaRepo.save(tema)).thenReturn(tema);
+        assert serviceWithMocks.saveStudent("1", "1", 912) == 0;
+        assert serviceWithMocks.saveTema("1123230", "tema 1", 3, 1) == 0;
     }
 }
--- src/test/java/GradeServiceTest.java
+++ src/test/java/GradeServiceTest.java
@@ -1,8 +1,14 @@
 import domain.Nota;
+import domain.Pair;
 import domain.Student;
 import domain.Tema;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import repository.*;
 import service.Service;
 import validation.NotaValidator;
@@ -10,8 +16,32 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.Validator;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class GradeServiceTest {
     private Service service;
+
+    @Mock
+    Validator<Student> studentValidatorMock;
+    @Mock
+    Validator<Tema> temaValidatorMock;
+    @Mock
+    Validator<Nota> notaValidatorMock;
+    @Mock
+    StudentRepository mockedStudentRepo;
+
+    @Mock
+    NotaRepository mockedNotaRepo;
+
+    @Mock
+    TemaRepository mockedTemaRepo;
+
+    @InjectMocks
+    Service serviceWithMocks;
+
     @Before
     public void setup(){
         Validator<Student> studentValidator = new StudentValidator();
@@ -28,6 +58,8 @@ public class GradeServiceTest {
         this.service = new Service(studentRepository, temaRepository, notaRepository);
         service.saveStudent("5", "teo", 935);
         service.saveTema("5", "Tema nr 5", 10, 1);
+
+        serviceWithMocks = new Service(mockedStudentRepo, mockedTemaRepo, mockedNotaRepo);
     }
 
     @Test
@@ -37,8 +69,25 @@ public class GradeServiceTest {
 
     @Test
     public void integrationFlowSuccessTest() {
-        assert service.saveStudent("12", "moni", 935) == 0;
-        assert service.saveTema("12", "Tema nr 12", 5, 2) == 0;
-        assert service.saveNota("12", "12", 10, 3, "Top") == 1;
+
+        Student student = new Student("1", "1", 912);
+        Tema tema = new Tema("1123230", "tema 1", 3, 1);
+        Nota nota = new Nota(new Pair<>("1", "1123230"), 10, 3, "Top");
+        doNothing().when(studentValidatorMock).validate(any());
+        doNothing().when(temaValidatorMock).validate(any());
+        doNothing().when(notaValidatorMock).validate(any());
+        Mockito.when(mockedStudentRepo.exists("1")).thenReturn(false);
+        Mockito.when(mockedTemaRepo.exists(tema.getID())).thenReturn(false);
+        Mockito.when(mockedTemaRepo.exists(tema.getID())).thenReturn(false);
+        Mockito.when(mockedStudentRepo.save(student)).thenReturn(student);
+        Mockito.when(mockedTemaRepo.save(tema)).thenReturn(tema);
+        Mockito.when(mockedTemaRepo.save(tema)).thenReturn(tema);
+        Mockito.when(mockedStudentRepo.findOne(anyString())).thenReturn(student);
+        Mockito.when(mockedTemaRepo.findOne(tema.getID())).thenReturn(tema);
+
+        assert serviceWithMocks.saveStudent("1", "1", 912) == 0;
+        assert serviceWithMocks.saveTema("1123230", "tema 1", 3, 1) == 0;
+        assert serviceWithMocks.saveNota("1", "1123230", 10, 3, "Top") == 1;
+
     }
 }
